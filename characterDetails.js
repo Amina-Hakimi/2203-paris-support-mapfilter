2f06aa26f538e92e1866a8fbf7de33aabc95f3ad
LoicBrassart <loic.brassart@gmail.com>
2022-04-06 11:23:03 +0200

    Initial structure

---
+++ characterDetails.js
@@ -0,0 +1,78 @@
+const character = {
+  id: 1,
+  name: '36AIS',
+  tier: 'SS',
+  weapon: 'Staff',
+  attribute: 'Healing',
+  species: 'Oxsecian',
+  friends: [
+    {
+      id: 3,
+      name: "A'merpact",
+      tier: 'A',
+      weapon: 'Staff',
+      attribute: 'Ice',
+      species: 'Lizardfolk',
+    },
+    {
+      id: 7,
+      name: 'Alika',
+      tier: 'S',
+      weapon: 'Bow',
+      attribute: 'Lightning',
+      species: 'Human',
+    },
+    {
+      id: 23,
+      name: 'Bahl',
+      tier: 'B',
+      weapon: 'Sword',
+      attribute: null,
+      species: 'Human',
+    },
+    {
+      id: 26,
+      name: "Beau'ne",
+      tier: 'Z',
+      weapon: 'Sword',
+      attribute: 'Photon',
+      species: 'Lizardfolk',
+    },
+    {
+      id: 32,
+      name: 'Calista',
+      tier: 'S',
+      weapon: 'Sword',
+      attribute: null,
+      species: 'Human',
+    },
+  ],
+  enemies: [
+    {
+      id: 19,
+      name: "Ba'gunar",
+      tier: 'B',
+      weapon: 'Sword',
+      attribute: 'Lightning',
+      species: 'Lizardfolk',
+    },
+    {
+      id: 37,
+      name: 'Clara',
+      tier: 'SS',
+      weapon: 'Spear',
+      attribute: null,
+      species: 'Human',
+    },
+    {
+      id: 39,
+      name: 'Czekras',
+      tier: 'SS',
+      weapon: 'Sword',
+      attribute: 'Ice',
+      species: 'Human',
+    },
+  ],
+};
+
+module.exports = character;
